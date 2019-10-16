9eb9f1e5707c8cd51accd0184bbda8977724e92c
G_MED <mohamed.guerfal.1995@gmail.com>
2019-10-16 12:41:22 +0200

    AccountKata

---
+++ src/main/java/com/baz/BankAccountKataApplication.java
@@ -0,0 +1,20 @@
+package com.baz;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BankAccountKataApplication implements CommandLineRunner{
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountKataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+	}
+}
---
+++ src/test/java/com/baz/BankAccountKataApplicationTests.java
@@ -0,0 +1,78 @@
+package com.baz;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.baz.entities.Operation;
+import com.baz.service.IBankService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BankAccountKataApplicationTests {
+
+	private static Logger logger = Logger.getLogger(BankAccountKataApplicationTests.class);
+
+	@Autowired
+	private IBankService bankService;
+
+	@Before
+	public void initTests() {
+		//clean Operations
+		bankService.deleteOperations();
+
+	}
+
+	@Test
+	public void testDeposit() {
+
+		// deposit
+		double amount = 200;
+		bankService.deposit(amount);
+		//select operations
+		List<Operation> list = bankService.listOperation();
+		assertTrue(list.size() == 1);
+		Operation op = list.get(0);
+		assertTrue(op.getAmount() == amount);
+		assertTrue(op.getType_op().equals("D"));
+		
+
+	}
+
+	@Test
+	public void testWithdrawal() {
+		// withdrawal
+		double amount = 100;
+		bankService.withdrawal(amount);
+		//select operations
+		List<Operation> list = bankService.listOperation();
+		assertTrue(list.size() == 1);
+		Operation op = list.get(0);
+		assertTrue(op.getAmount() == amount);
+		assertTrue(op.getType_op().equals("W"));
+
+	}
+
+	@Test
+	public void testListOperation() {
+		
+		//add operation
+		bankService.deposit(5000);
+		bankService.withdrawal(500);
+		bankService.withdrawal(500);
+		//list all operations
+		List<Operation> list = bankService.listOperation();
+		assertEquals(3, list.size());
+
+	}
+
+}
